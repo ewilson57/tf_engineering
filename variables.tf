93fe1e0e9b21c633ee93119e23fa0d30f7c29d42
Earl Wilson <ewilson57@gmail.com>
2023-03-11 21:46:39 -0500

    updated variables

--- variables.tf
+++ variables.tf
@@ -34,10 +34,10 @@ variable "tags" {
   }
 }
 
-variable "arm_client_id" {}
-variable "arm_client_secret" {}
-variable "arm_tenant_id" {}
-variable "arm_subscription_id" {}
+variable "ARM_CLIENT_ID" {}
+variable "ARM_CLIENT_SECRET" {}
+variable "ARM_TENANT_ID" {}
+variable "ARM_SUBSCRIPTION_ID" {}
 variable "router_wan_ip" {}
 variable "admin_username" {}
 variable "admin_password" {}
