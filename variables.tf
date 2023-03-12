6204efaffcc91dc271b2e3fa6939681645e02ad9
Earl Wilson <ewilson57@gmail.com>
2023-03-11 21:56:48 -0500

    updated variables/added .env file

--- variables.tf
+++ variables.tf
@@ -34,10 +34,6 @@ variable "tags" {
   }
 }
 
-variable "ARM_CLIENT_ID" {}
-variable "ARM_CLIENT_SECRET" {}
-variable "ARM_TENANT_ID" {}
-variable "ARM_SUBSCRIPTION_ID" {}
 variable "router_wan_ip" {}
 variable "admin_username" {}
 variable "admin_password" {}
